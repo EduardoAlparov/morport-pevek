8fb8281275f7579722fc31abe806aecd9bb42c18
EduardoAlparov <workman133@yandex.ru>
2022-12-13 14:54:49 +0300

    repair contacts, error, history 13.12.22

--- src/js/modules/yandexMap.js
+++ src/js/modules/yandexMap.js
@@ -1,36 +1,67 @@
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default () => {
-  let  myMap;
   const mapWrapper = document.getElementById('ya-map');
 
-  if (mapWrapper) {
-    ymaps.ready(init);
+  let mapScrolTrigge = ScrollTrigger.create({
+    start: 0,
+    end: "max",
+    onUpdate: (self) => mapInViewField(),
+    once: true
+  });
 
-		function init () {
-			myMap = new ymaps.Map('ya-map', {
-				center: [69.703364, 170.264721], // Pevek port
-				zoom: 12,
-        controls: [],
-        behaviors: [],
-			}, {});
+  function mapInViewField() {
+    const script = document.createElement('script');
 
-      myMap.geoObjects.add(
-        new ymaps.Placemark([69.702974, 170.274369], {
-      }, {
-            draggable: false,
-            iconLayout: ymaps.templateLayoutFactory.createClass([
-              '<div class="ya-maps-placemark"">',
-              '{% include "default#image" %}',
-              '</div>'
-            ].join('')),
-            // Своё изображение иконки метки.
-            iconImageHref: 'assets/images/map/anchor.svg',
-            // Размеры метки.
-            iconImageSize: [52, 52],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-26, -26]
-      })
-      )
-		}
+    if(ScrollTrigger.isInViewport(mapWrapper)) {
+      script.src = "https://api-maps.yandex.ru/2.1/?apikey=d02525f1-2a0d-4700-a5e1-e4487f06702c&?apikey=d02525f1-2a0d-4700-a5e1-e4487f06702c&load=package.full&lang=ru-RU";
+      document.head.append(script);
+      mapScrolTrigge.disable();
+    }
+
+    script.onload = function() {
+      let  myMap;
+      let centerCoordinates = JSON.parse("[" + mapWrapper.dataset.centerCoord + "]");
+      let placemarkCoordinates = JSON.parse("[" + mapWrapper.dataset.placemarkCoord + "]");
+      let mapZoom = Number(mapWrapper.dataset.zoom);
+      let iconHref = mapWrapper.dataset.iconHref;
+      let mapControls = JSON.parse("[" + mapWrapper.dataset.controls + "]");
+
+      if (mapWrapper) {
+        ymaps.ready(init);
+
+        function init () {
+          myMap = new ymaps.Map('ya-map', {
+            center: centerCoordinates, // Pevek port
+            zoom: mapZoom,
+            controls: mapControls,
+            behaviors: [],
+          }, {});
+
+          myMap.geoObjects.add(
+            new ymaps.Placemark( placemarkCoordinates, {
+          }, {
+                draggable: false,
+                iconLayout: ymaps.templateLayoutFactory.createClass([
+                  '<div class="ya-maps-placemark"">',
+                  '{% include "default#image" %}',
+                  '</div>'
+                ].join('')),
+                // Своё изображение иконки метки.
+                iconImageHref: iconHref,
+                // Размеры метки.
+                iconImageSize: [52, 52],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-26, -26]
+          })
+          )
+        }
+      }
+    }
   }
 }
