33ab1af992ad861966981b78715cf7a709e0f49b
EduardoAlparov <workman133@yandex.ru>
2022-12-23 17:22:13 +0300

    new fix and vertical history page 23.12.22

--- src/js/modules/infoButtonsListSlider.js
+++ src/js/modules/infoButtonsListSlider.js
@@ -5,6 +5,12 @@ export default () => {
     return;
   }
 
+  let iniaitalActiveLink = infoNav.querySelector('.nav__item--active');
+  if(iniaitalActiveLink) {
+    let iniaitalOffsetItem = iniaitalActiveLink.offsetLeft;
+    iniaitalActiveLink.closest('.nav__list').scrollLeft = iniaitalOffsetItem;
+  }
+
   infoNav.querySelectorAll('.nav__item').forEach( item => {
     item.addEventListener('click', (e) => {
       infoNav.querySelectorAll('.nav__item').forEach( i => {
--- src/js/modules/schemeTextControl.js
+++ src/js/modules/schemeTextControl.js
@@ -2,15 +2,24 @@ export default () => {
   const schemeWrapper = document.querySelector('.scheme__inner-wrapper');
   const schemeMobileText = document.querySelector('.scheme__mobile-desc');
   const schemeButton = document.querySelectorAll('.scheme__button');
+  const emptyInput = document.getElementById('empty');
+
 
   if(schemeWrapper && (window.matchMedia("(max-width: 991px)").matches)) {
     schemeWrapper.querySelectorAll('input[type="radio"]').forEach(input => {
-      input.addEventListener('input', () => {
+      input.addEventListener('change', () => {
         if(getLabel(input.id).classList.contains('scheme__mask')) {
           schemeMobileText.innerHTML = '';
           schemeMobileText.innerHTML = getLabel(input.id).parentElement.querySelector('.scheme__desc-wrapper').outerHTML;
         }
       })
+    })
+
+    schemeWrapper.addEventListener('click', (e) => {
+      if (e.target.closest('.scheme__pseudo-button')) {
+        emptyInput.checked = true;
+        schemeMobileText.innerHTML = '';
+      }
     })
   }
 
